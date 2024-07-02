a2d0776452812e1d909ab00c1c3c888bc2bc0963
unknown <ijazkhayam@gmail.com>
2024-07-02 19:48:54 +0500

    .

--- src/components/MainCont.jsx
+++ src/components/MainCont.jsx
@@ -6,7 +6,6 @@ const MainCont = () => {
       <div className="w-full h-auto flex flex-col mt-12 items-center justify-center">
         <img className='w-[80%]' src="image1.png" alt="" />
         <h1 className='text-white font-[poppins] font-bold text-3xl w-[90%] text-center m-auto'>Double Up Your Views on Youtube With Top Thumbnails</h1>
-        <p className='text-gray-300 text-center w-[90%] mt-8'>Unlock the power of first impressions with our expertly crafted thumbnails. At Pixel Creators, we blend creativity with precision to transform ideas into captivating visuals that demand attention.</p>
         <button className='text-white mt-12 text-lg rounded-md font-semibold bg-gradient-to-br from-[#171233] via-[#43107e] to-[#171233] px-14 py-4'>Visit Portfolio</button>
       </div>
     </>
