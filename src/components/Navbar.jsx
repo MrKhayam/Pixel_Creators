4efa9476bb51f911d45fd61955853310d35e26f9
unknown <ijazkhayam@gmail.com>
2024-07-02 19:31:56 +0500

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
       <div className="m-auto w-[90%] rounded-lg h-auto p-4 bg-gradient-to-r from-[#1A143D] via-[#3B0F6C] to-[#1A143D] flex justify-between items-center">
       <div className="left">
       <div className="logo">
-        <img className='w-[25%]' src="../public/1.png" alt="" />
+        <img className='w-[25%]' src="/1.png" alt="" />
       </div>
       </div>
       <div className="right text-white">
