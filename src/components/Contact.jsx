3610894e627145db30509d367c4f37b09b195b92
unknown <ijazkhayam@gmail.com>
2024-07-03 17:06:27 +0500

    .

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,8 +9,8 @@ const Contact = () => {
             <h1 className='text-white text-center text-3xl w-[80%] font-bold'>Get Access To The Right People</h1>
         </div>
         <div className="form flex flex-col gap-1 items-center justify-center w-[90%] h-auto ">
-        <button className='text-white mt-0 text-lg rounded-md font-semibold bg-gradient-to-br from-[#171233] via-[#43107e] to-[#171233] px-28 py-4 cursor-default'>Let's Talk</button>
-        <button disabled className='text-white mt-4 text-lg rounded-md font-semibold cursor-pointer bg-green-600 px-[108px] py-4 transition-all duration-150 hover:scale-95'>Whatsapp</button>
+        <button className='text-white mt-0 text-lg rounded-md font-semibold bg-gradient-to-br from-[#171233] via-[#43107e] to-[#171233]  w-72 py-4 cursor-default'>Let's Talk</button>
+        <button disabled className='text-white mt-4 text-lg rounded-md font-semibold cursor-pointer bg-green-600 px-[103px] py-4 transition-all duration-150 hover:scale-95'>Whatsapp</button>
         </div>
       </div>
     </>
